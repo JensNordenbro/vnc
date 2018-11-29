b25cb0cd6ad39038744aad6b1559a8448406f1ac
Jens Nordenbro <jnb@beijer.se>
2018-11-29 15:00:24 +0100

    bind ipv4

--- ServerServerProxy/Program.cs
+++ ServerServerProxy/Program.cs
@@ -12,6 +12,6 @@ namespace VncDeviceProxyCloudSide
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>().UseUrls("http://0.0.0.0:80");
     }
 }
